e8ff812cd9530c7f954a9dff63a3de0bc41bde1d
Kiran Giri <kirangiri2298@gmail.com>
2024-05-29 13:06:58 +0545

    created login page

---
+++ lib/screens/auth/login_screen.dart
@@ -0,0 +1,76 @@
+import 'package:chat_app/main.dart';
+import 'package:chat_app/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _isAnimate = false;
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        _isAnimate = true;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    mq = MediaQuery.of(context).size;
+    return Scaffold(
+      //app bar//
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Welcome to We Chat'),
+      ),
+      body: Stack(
+        children: [
+          AnimatedPositioned(
+              top: mq.height * 0.15,
+              right: _isAnimate ? mq.width * .25 : -mq.width * .5,
+              width: mq.width * .5,
+              duration: const Duration(seconds: 1),
+              child: Image.asset('assets/images/chat.png')),
+          Positioned(
+              bottom: mq.height * 0.15,
+              left: mq.width * .05,
+              width: mq.width * .9,
+              height: mq.height * .06,
+              child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 219, 255, 178),
+                    shape: const StadiumBorder(),
+                    elevation: 1,
+                  ),
+                  onPressed: () {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (_) => const HomeScreen()));
+                  },
+                  icon: Image.asset(
+                    'assets/images/google.png',
+                    height: mq.height * .03,
+                  ),
+                  label: RichText(
+                      text: const TextSpan(
+                          style: TextStyle(color: Colors.black, fontSize: 19),
+                          children: [
+                        TextSpan(text: 'Login with '),
+                        TextSpan(
+                            text: 'Google',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.green))
+                      ])))),
+        ],
+      ),
+    );
+  }
+}
